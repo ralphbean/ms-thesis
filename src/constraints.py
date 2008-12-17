efd03536b1b774f0cb14c14383c2456974ac0f18
Ralph Bean <ralph.bean@gmail.com>
2008-12-16 20:58:23 -0500

    Updated the existing print-tests to actually test things.

--- src/constraints.py
+++ src/constraints.py
@@ -7,10 +7,15 @@
 # Takes a tree as a dictionary and prints its nodes inorder
 def tree_to_inorder_string(root):
     retval = ""
+    if ( len(root.keys()) > 1 ):
+        retval = retval + "("
+
     if ( root.__contains__('left') ):
         retval = retval + tree_to_inorder_string(root['left'])
 
+    # This must exist, or we have an invalid tree
     data = root['data']
+
     # Data can be either a variable, constant, or operator
     if data['type'] == 'operator':
         retval = retval + data['value']
@@ -23,22 +28,25 @@ def tree_to_inorder_string(root):
 
     if ( root.__contains__('right') ):
         retval = retval + tree_to_inorder_string(root['right'])
+    if ( len(root.keys()) > 1 ):
+        retval = retval + ")"
 
     return retval
 
 # Takes the dictionary structure of a set of contraints and prints them in
 #  human-readable form
 def constraints_to_string( constraints ):
-    n = constraints['n']
-    retval = "Constraints.   n =" + str(n)
-    for c in constraints['eqns']:
-        retval = retval + "\n" + tree_to_inorder_string(c)
-    return retval
+    s = "n:" + str(constraints['n']) + "\n"
+    s = s + "\n".join([tree_to_inorder_string(c) for c in constraints['eqns']])
+    return s
 
 # Takes the dictionary structure of a system input and prints it in
 #  human-readable form
 def input_to_string( input ):
-    print "TODO"
+    s = "x0:" + str(input['x0']) + "\n"
+    s = s + "dx:" + str(input['dx']) + "\n"
+    s = s + tree_to_inorder_string(input['eqn'])
+    return s
 
 # Returns true if a system satisfies the given set of constraints and associated
 #  input
--- src/unittest_constraints.py
+++ src/unittest_constraints.py
@@ -4,8 +4,8 @@ import unittest
 import constraints
 
 class TestConstraints(unittest.TestCase):
-    def testPrintConstraints(self):
-        system = { 'n' : 2,
+    def setUp(self):
+        self.system = { 'n' : 2,
                 'eqns' : [
                     { 'data'  :
                         {'type'  : 'operator',
@@ -24,9 +24,36 @@ class TestConstraints(unittest.TestCase):
                                       {'type' : 'constant',
                                           'value' : '2'}
                                       }}}]}
-        print constraints.constraints_to_string(system)
+        self.input = {  'x0'  : 0,
+                        'dx'  : 1,
+                        'eqn' :
+                    { 'data'  :
+                        {'type'  : 'operator',
+                         'value' : '*'},
+                      'left'  : { 'data' : 
+                                    { 'type' : 'parameter',
+                                      'value' : '1' }},
+                      'right' : { 
+                          'data' : {'type'  : 'operator',
+                         'value' : '+'},
+                                  'left' : { 'data' : 
+                                      {'type' : 'constant',
+                                          'value' : '3'}
+                                      },
+                                  'right': { 'data' : 
+                                      {'type' : 'constant',
+                                          'value' : '2'}
+                                      }}}}
+
+    def testPrintConstraints(self):
+        s = constraints.constraints_to_string(self.system)
+        self.assertTrue(s == "n:2\n(a_{1}=(1+2))")
 
     def testPrintInput(self):
+        s = constraints.input_to_string(self.input)
+        self.assertTrue(s == "x0:0\ndx:1\n(a_{1}*(3+2))")
+
+    def testIterateInput(self):
         pass
     def testSatisfactory(self):
         pass
